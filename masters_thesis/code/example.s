04c0f71ec2544d7ac940973919f6d715305f2043
Tomas Meszaros <exo@tty.sk>
2018-12-01 21:55:34 +0100

    edit up to call graph

--- masters_thesis/code/example.s
+++ masters_thesis/code/example.s
@@ -1,51 +1,83 @@
-; ModuleID = 'example.c'
-source_filename = "example.c"
-target datalayout = "e-m:e-i64:64-f80:128-n8:16:32:64-S128"
-target triple = "x86_64-unknown-linux-gnu"
+	.text
+	.file	"example.c"
+	.globl	foo                     # -- Begin function foo
+	.p2align	4, 0x90
+	.type	foo,@function
+foo:                                    # @foo
+	.cfi_startproc
+# BB#0:                                 # %entry
+	pushq	%rbp
+.Lcfi0:
+	.cfi_def_cfa_offset 16
+.Lcfi1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+.Lcfi2:
+	.cfi_def_cfa_register %rbp
+	movl	%edi, -4(%rbp)
+	movl	-4(%rbp), %edi
+	addl	$10, %edi
+	movl	%edi, -8(%rbp)
+	movl	-8(%rbp), %eax
+	popq	%rbp
+	retq
+.Lfunc_end0:
+	.size	foo, .Lfunc_end0-foo
+	.cfi_endproc
+                                        # -- End function
+	.globl	bar                     # -- Begin function bar
+	.p2align	4, 0x90
+	.type	bar,@function
+bar:                                    # @bar
+	.cfi_startproc
+# BB#0:                                 # %entry
+	pushq	%rbp
+.Lcfi3:
+	.cfi_def_cfa_offset 16
+.Lcfi4:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+.Lcfi5:
+	.cfi_def_cfa_register %rbp
+	movl	$42, -4(%rbp)
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	retq
+.Lfunc_end1:
+	.size	bar, .Lfunc_end1-bar
+	.cfi_endproc
+                                        # -- End function
+	.globl	main                    # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# BB#0:                                 # %entry
+	pushq	%rbp
+.Lcfi6:
+	.cfi_def_cfa_offset 16
+.Lcfi7:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+.Lcfi8:
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	movl	$10, -8(%rbp)
+	movl	-8(%rbp), %edi
+	callq	foo
+	movl	%eax, -12(%rbp)
+	callq	bar
+	xorl	%edi, %edi
+	movl	%eax, -16(%rbp)
+	movl	%edi, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+.Lfunc_end2:
+	.size	main, .Lfunc_end2-main
+	.cfi_endproc
+                                        # -- End function
 
-; Function Attrs: noinline nounwind optnone uwtable
-define i32 @foo(i32 %n) #0 {
-entry:
-  %n.addr = alloca i32, align 4
-  %x = alloca i32, align 4
-  store i32 %n, i32* %n.addr, align 4
-  %0 = load i32, i32* %n.addr, align 4
-  %add = add nsw i32 %0, 10
-  store i32 %add, i32* %x, align 4
-  %1 = load i32, i32* %x, align 4
-  ret i32 %1
-}
-
-; Function Attrs: noinline nounwind optnone uwtable
-define i32 @bar() #0 {
-entry:
-  %y = alloca i32, align 4
-  store i32 42, i32* %y, align 4
-  %0 = load i32, i32* %y, align 4
-  ret i32 %0
-}
-
-; Function Attrs: noinline nounwind optnone uwtable
-define i32 @main() #0 {
-entry:
-  %retval = alloca i32, align 4
-  %some_int = alloca i32, align 4
-  %foo_result = alloca i32, align 4
-  %bar_result = alloca i32, align 4
-  store i32 0, i32* %retval, align 4
-  store i32 10, i32* %some_int, align 4
-  %0 = load i32, i32* %some_int, align 4
-  %call = call i32 @foo(i32 %0)
-  store i32 %call, i32* %foo_result, align 4
-  %call1 = call i32 @bar()
-  store i32 %call1, i32* %bar_result, align 4
-  ret i32 0
-}
-
-attributes #0 = { noinline nounwind optnone uwtable "correctly-rounded-divide-sqrt-fp-math"="false" "disable-tail-calls"="false" "less-precise-fpmad"="false" "no-frame-pointer-elim"="true" "no-frame-pointer-elim-non-leaf" "no-infs-fp-math"="false" "no-jump-tables"="false" "no-nans-fp-math"="false" "no-signed-zeros-fp-math"="false" "no-trapping-math"="false" "stack-protector-buffer-size"="8" "target-cpu"="x86-64" "target-features"="+fxsr,+mmx,+sse,+sse2,+x87" "unsafe-fp-math"="false" "use-soft-float"="false" }
-
-!llvm.module.flags = !{!0}
-!llvm.ident = !{!1}
-
-!0 = !{i32 1, !"wchar_size", i32 4}
-!1 = !{!"clang version 5.0.1 (tags/RELEASE_500/final)"}
+	.ident	"clang version 5.0.1 (tags/RELEASE_500/final)"
+	.section	".note.GNU-stack","",@progbits
